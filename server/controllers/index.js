e3ae9b526b7599c5f1afbd2b85b27f2ad0344f9e
khaled <yonkoluffy48@gmail.com>
2022-03-02 08:21:09 +0200

    handle addNotes route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,11 @@ const getNoteData = require('./getData');
 const postNoteData = require('./postData');
 const deleteNote = require('./deleteData');
 
+const addNoteHandle = require('./addNoteHandle');
+
 const router = express.Router();
 
+router.get('/addNotes', addNoteHandle);
 router.get('/getData', getNoteData);
 router.post('/addNote', postNoteData);
 router.use('/deleteData', deleteNote);
